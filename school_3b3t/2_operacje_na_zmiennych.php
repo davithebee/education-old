4fac33d9257417bdca1580d698568a41e1431b44
liberskidawid <70701439+liberskidawid@users.noreply.github.com>
2020-09-24 14:50:50 +0200

    Update 2_operacje_na_zmiennych.php

--- school_3b3t/2_operacje_na_zmiennych.php
+++ school_3b3t/2_operacje_na_zmiennych.php
@@ -73,4 +73,79 @@
   //jeżeli jest równa, ma wartość 0
   //jeżeli jest większa, ma wartość 1
 
+
+  echo "<hr>";
+
+  $x = 1;
+  $y = 1.0;
+
+  //porównanie czy jest równe
+  if ($x == $y){
+    echo "$x jest równe $y";
+  }else{
+    echo "$x nie jest równe $y";
+  }
+
+  echo "<br>";
+
+  //porównanie czy jest identyczne
+  if ($x === $y){
+    echo "$x jest identyczne $y";
+  }else{
+    echo "$x nie jest identyczne $y";
+  }
+
+  echo "<br>";
+  echo gettype($x); echo "<br>"; //integer
+  echo gettype($y); echo "<br>"; //double
+  echo "<hr>";
+  /*
+  preinkrementacja ++$i
+  predekrementacja --$i
+  postinkrementacja $i++
+  postdekrementacja $i--
+  */
+
+  $x=5;
+  $x=$x++; /*5*/
+  echo $x; echo"<br>";
+  $x=++$x; /*7*/
+  echo $x; echo"<br>";
+  $y=$x++;
+  echo $x; /*7*/ echo"<br>";
+  echo $y; /*6*/ echo"<br>";
+  $y=++$x*2-1;
+  echo $x; /*8*/ echo"<br>";
+  echo $y; /*15*/ echo"<br>";
+
+  //operatory rzutowania
+  //bool, int, float, string, array, object, unset
+
+  $test1 = '123abc45';
+  echo "<hr> $test1";
+  echo '<br>Typ danych $test1: ',gettype($test1),'<br>';
+
+  $test1 = (int)$test1;
+  echo "<hr> $test1"; //po dojściu do stringa (w tym przypadku litery a) kończy rzutować
+  echo '<br>Typ danych $test1: ',gettype($test1),'<br>';
+
+  $test2 = 0;
+  echo "<hr> $test2";
+  echo '<br>Typ danych $test2: ',gettype($test2),'<br>';
+
+  $test2 = (bool)$test2;
+  echo "<hr> $test2";
+  echo '<br>Typ danych $test2: ',gettype($test2),'<br>';
+
+  $test3 = 20.5;
+  echo "<hr> $test3";
+  echo '<br>Typ danych $test3: ',gettype($test3),'<br>';
+
+  $test3 = (unset)$test3;
+  echo "<hr> $test3";
+  echo '<br>Typ danych $test3: ',gettype($test3),'<hr>';
+
+  //rozmiar typu integer
+  echo PHP_INT_SIZE; //8
  ?>
+
