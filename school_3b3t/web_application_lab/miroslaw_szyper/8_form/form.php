55e1585138c2872dd2fbec2a821c783b1599e1fb
liberskidawid <dliberski7@gmail.com>
2020-10-22 13:50:28 +0200

    Finishing lesson

--- school_3b3t/web_application_lab/miroslaw_szyper/8_form/form.php
+++ school_3b3t/web_application_lab/miroslaw_szyper/8_form/form.php
@@ -8,9 +8,14 @@
     <h4>Dane użytkownika</h4>
     <!-- <form action="./form_dane.php" method="post" novalidate -->
     <form action="./form_dane.php" method="post">
-      <input type="text" name="surname" placeholder="Nazwisko" autofocus autocomplete="off" required pattern="[a-z]{2,5}">
+      <input type="text" name="surname" placeholder="Nazwisko" autofocus autocomplete="off" required pattern="[a-ząęśżźćńłó]{2,5}">
       <br><br>
-
+      <select name="sex">
+        <option disable hidden selected>Wybierz opcję</option>
+        <option value="m">Mężczyzna</option>
+        <option value="f">Kobieta</option>
+        <!-- <option value="f" selected>Kobieta</option> -->
+      </select>
       <input type="submit" value="Wyślij Dane">
     </form>
   </body>
---
+++ school_3b3t/web_application_lab/miroslaw_szyper/8_form/form_dane.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+	<meta charset="utf-8">
+	<title>Document</title>
+</head>
+<body>
+	<h4>Dane użytkownika</h4>
+	<?php
+		print_r($_POST);
+	?>
+</body>
+</html>
