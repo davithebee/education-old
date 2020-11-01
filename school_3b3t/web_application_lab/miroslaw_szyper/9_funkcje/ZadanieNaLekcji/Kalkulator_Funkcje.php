53f99a36cea9bcd050663062ea83413501b9e1da
liberskidawid <dliberski7@gmail.com>
2020-11-01 22:30:44 +0100

    Homework 01.11.2020 - poprawka

--- school_3b3t/web_application_lab/miroslaw_szyper/9_funkcje/ZadanieNaLekcji/Kalkulator_Funkcje.php
+++ school_3b3t/web_application_lab/miroslaw_szyper/9_funkcje/ZadanieNaLekcji/Kalkulator_Funkcje.php
@@ -2,14 +2,24 @@
 function add($x,$y){
   return $x + $y;
 }
+// Funkcja add - dodaje dwie wartości przywołane z głównego pliku.
 function substract($x,$y){
   return $x - $y;
 }
+// Funkcja substract - dodaje dwie wartości przywołane z głównego pliku.
 function multiply($x,$y){
   return $x * $y;
 }
+// Funkcja multiply - dodaje dwie wartości przywołane z głównego pliku.
 function divide($x,$y){
   if($y==0) return 'ERROR';
   return $x / $y;
 }
+// Funkcja divide - dodaje dwie wartości przywołane z głównego pliku.
 ?>
+
+<!-- Definicja Funkcji - Funkcja to zbiór instrukcji,
+który posiada nazwę, zbiór argumentów (ale nie musi posiadać argumentów)
+i znajduje się pomiędzy nawiasami {}.
+Pozwala na wyodrębnienie fragmentu kodu programu w celu uniknięcia powtórzeń
+i może być wielokrotnie wywoływana w kodzie.-->
