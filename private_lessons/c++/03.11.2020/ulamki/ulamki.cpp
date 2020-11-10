ae1ac234b3c3be7a456c459a0a06ae8592cab0ac
liberskidawid <70701439+liberskidawid@users.noreply.github.com>
2020-11-10 17:01:01 +0100

    Add files via upload

--- private_lessons/c++/03.11.2020/ulamki/ulamki.cpp
+++ private_lessons/c++/03.11.2020/ulamki/ulamki.cpp
@@ -2,12 +2,23 @@
 #include <fstream>
 using namespace std;
 
+int NWD(int a, int b){
+  int r;
+  while(b>0){
+    r=a%b;
+    a=b;
+    b=r;
+  }
+  return a;
+}
+
 int main()
 {
   int licz[1000], mian[1000];
   fstream dane_ulamki, wyniki;
 
 //-------WCZYTANIE-Z-PLIKU-------------------
+
   dane_ulamki.open("dane_ulamki.txt", ios::in);
   wyniki.open("wyniki.txt", ios::out);
 
@@ -23,7 +34,8 @@ int main()
   }
 
   dane_ulamki.close();
-//--------------------------------------------
+
+//-------ZADANIE-1---------------------------
 
 int licz_min=licz[0];
 int mian_min=mian[0];
@@ -44,6 +56,27 @@ for(int i=1; i<1000; i++){
 wyniki<<"ZADANIE 1"<<endl;
 wyniki<<"Licznik minimalny: "<<licz_min<<endl;
 wyniki<<"Mianownik minimalny: "<<mian_min<<endl<<endl;
+
+//-------ZADANIE-2---------------------------
+
+int count=0;
+for(int i=0; i<1000; i++){
+  if(NWD(licz[i],mian[i])==1) count++;
+}
+
+wyniki<<"ZADANIE 2"<<endl;
+wyniki<<"Ilosc nieskracalnych ulamkow: "<<count<<endl<<endl;
+
+//-------ZADANIE-3---------------------------
+
+int suma=0;
+for(int i=0; i<1000; i++){
+  suma+=(licz[i]/NWD(licz[i],mian[i]));
+  //np nwd z 15/3 jest rowne 5, wiec 15/5=3 (3/1)
+}
+
+wyniki<<"ZADANIE 3"<<endl;
+wyniki<<"Suma licznikow ulamkow nieskracalnych: "<<suma;
 
 return 0;
 }
