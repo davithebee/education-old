80833dd52fe585f62d6034f21d4de55450e7657f
liberskidawid <dliberski7@gmail.com>
2021-01-19 20:32:27 +0100

    .

--- private_lessons/c++/01.12.2020/tallestColumn.cpp
+++ private_lessons/c++/01.12.2020/tallestColumn.cpp
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-#include <iostream>
-
-int tallestColumn(int image[][320], int imageHeight, int imageWidth){
-
-int columnCount{0};
-int columnMaxSize{0};
-
-    for(int coordX{0}; coordX < imageWidth; coordX++)
-        {
-            for(int coordY{imageHeight-1}; coordY > 0; coordY--)
-                {
-                    if(image[coordY-1][coordX] == image[coordY][coordX])
-                    {
-                        columnCount++;
-
-                        if(columnMaxSize<columnCount)
-                        {
-                            columnMaxSize = columnCount;
-                        }
-                    }
-                    else
-                    {
-                        columnCount = 0;
-                    }
-                }
-        }
-
-        return columnMaxSize;
-}
-=======
 #include <iostream>
 
 int tallestColumn(int image[][320], int imageHeight, int imageWidth){
@@ -57,4 +26,3 @@ int maxColumnHeight{0};
 
         return maxColumnHeight;
 }
->>>>>>> 1dbeddd376a53d6f8df7b444f3fef835973e0fc9
