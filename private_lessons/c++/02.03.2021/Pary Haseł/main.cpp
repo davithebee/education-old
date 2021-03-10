969dcf7e3b1d7f3a21724d99fdec46f7f4385ef7
liberskidawid <70701439+liberskidawid@users.noreply.github.com>
2021-03-10 17:35:50 +0100

    Add files via upload

--- private_lessons/c++/02.03.2021/Pary Haseł/main.cpp
+++ private_lessons/c++/02.03.2021/Pary Haseł/main.cpp
@@ -17,13 +17,64 @@ int main()
         std::string leftPwd[1000];
         std::string rightPwd[1000];
         int anagramCount{0};
+        int alphabetLetterAmount[26]{0};
 
         for(int i{0}; i < 1000; i++)
         {
             in >> leftPwd[i];
+            for(int j{0}; j < leftPwd[i].size(); j++)
+                {
+                    alphabetLetterAmount[(int)leftPwd[i][j] - 65]++;
+                }
             in >> rightPwd[i];
+            for(int j{0}; j < rightPwd[i].size(); j++)
+                {
+                    alphabetLetterAmount[(int)rightPwd[i][j] - 65]++;
+                }
         }
 
+        /*for(int i{0}; i < 26; i++)
+            {
+                std::cout << char(i+65) << ": " << alphabetLetterAmount[i] << '\n';
+            }*/
+
+        int mostFreqLetter{0};
+        int leastFreqLetter{54056};
+
+        for(int i{0}; i < 26; i++)
+            {
+                if(alphabetLetterAmount[i] > mostFreqLetter)
+                {
+                    mostFreqLetter = alphabetLetterAmount[i];
+                }
+
+                if(alphabetLetterAmount[i] < leastFreqLetter)
+                {
+                    leastFreqLetter = alphabetLetterAmount[i];
+                }
+            }
+
+        int a{0};
+        int b{0};
+        int c{0};
+        int d{0};
+
+        for(int i{0}; i < 26; i++)
+            {
+                if(alphabetLetterAmount[i] == mostFreqLetter)
+                {
+                    a++;
+                    c = i;
+                }
+                if(alphabetLetterAmount[i] == leastFreqLetter)
+                {
+                    b++;
+                    d = i;
+                }
+            }
+
+        /*std::cout << mostFreqLetter <<'\n';
+        std::cout << leastFreqLetter << '\n';*/
 
         for(int i{0}; i < 1000; i++)
         {
@@ -31,11 +82,11 @@ int main()
         }
 
         out << "Zadanie 1 \n"
-            << "Ilo�� anagram�w: "
+            << "Ilość anagramów: "
             << anagramCount << '\n' << '\n';
 
         out << "Zadanie 2 \n"
-            << "Has�a zawieraj�ce co najmniej dwie tr�jki: \n";
+            << "Hasła zawierające co najmniej dwie trójki: \n";
 
         for(int i{0}; i < 1000; i++)
         {
@@ -54,7 +105,28 @@ int main()
         out << "BAC: " << ABC_fileCount[2] << '\n';
         out << "BCA: " << ABC_fileCount[3] << '\n';
         out << "CAB: " << ABC_fileCount[4] << '\n';
-        out << "CBA: " << ABC_fileCount[5] << '\n';
+        out << "CBA: " << ABC_fileCount[5] << '\n' << '\n';
+
+        out << "Zadanie 3 \n";
+
+        if(a == 1)
+        {
+           out << "Najczęściej wystepująca litera: " << char(c + 65) << '\n';
+        }
+        else
+        {
+           out << "Brak najczęściej wystepującej litery.\n";
+        }
+        if(b == 1)
+        {
+           out << "Najrzadziej wystepujaca litera: " << char(d + 65) << '\n';
+        }
+        else
+        {
+           out << "Brak najrzadziej wystepujacej litery.\n";
+        }
+
+
 
         return 0;
     }
